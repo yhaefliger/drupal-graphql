d88b5fce955c2a0947e2f24269c3b437a92e24ca
Yann Haefliger <Yann.Haefliger@sib.swiss>
2022-10-22 10:48:38 +0200

    fix warning offset value

--- src/Routing/QueryRouteEnhancer.php
+++ src/Routing/QueryRouteEnhancer.php
@@ -31,7 +31,7 @@ class QueryRouteEnhancer implements EnhancerInterface {
     // graphql processor due to a missing query string but at least provides
     // the right format for the client to act upon.
     return $defaults + [
-      '_controller' => $defaults['_graphql']['single'],
+      '_controller' => $defaults['_graphql']['single'] ?? NULL,
       'operations' => $operations,
     ];
   }
